544e3733bf7f0d7c8a36ba31f7830457cdae7f7b
Eddie Lantoniaina Ramanitrandrasana <eramanit@c1r11s2.42antananarivo.mg>
2025-03-29 10:27:37 +0300

    tester du ft_printf sur le vrai printf

---
+++ ft_printf_tester/main.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eramanit <eramanit@student.42antananari    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/29 06:56:41 by eramanit          #+#    #+#             */
+/*   Updated: 2025/03/29 07:34:26 by eramanit         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+
+// printf("\033[32mBonjour\033[00m\n");
+// [ couleur m
+int main(void)
+{
+	int c = 78;
+	char	*s = "N";
+	puts("the printf:");
+	printf("le caractere \033[35m\"%c\"\033[00m/", 'c');
+	puts("");
+
+	puts("the printf:");
+	printf("le char * \033[35m\"%s\"\033[00m/", "chaine");
+	puts("");
+
+	puts("the printf:");
+	printf("le decimal \033[35m\"%d\"\033[00m/", 0xf);
+	puts("");
+
+	puts("the printf:");
+	printf("l'entier: \033[35m\"%i\"\033[00m/", -1);
+	puts("");
+
+	puts("the printf:");
+	printf("l'entier naturel: \033[35m\"%u\"\033[00m/", -1);
+	puts("");
+
+	puts("the printf:");
+	printf("les entiers hex: \033[35m\"%x %x\"\033[00m/", -16, 15);
+	puts("");
+
+
+	puts("the printf:");
+	printf("les entiers HEX: \033[35m\"%i = %X\"\033[00m/", 15, 15);
+	puts("");
+
+	puts("the printf:");
+	printf("le mix de types: \033[35m%%%s = %dd = %ii = %xx = %cc = %XX stocked dans: %p%%\033[00m/",
+			s, c, c, c,(char) c, c, &c);
+	puts("");
+	return (0);
+}
