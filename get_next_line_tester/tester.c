a7956f67ffa40178859c95bc48a0e4e2045be8e5
Eddie Lantoniaina Ramanitrandrasana <eramanit@c1r11s2.42antananarivo.mg>
2025-04-13 21:37:08 +0300

    stylisation de tester

--- get_next_line_tester/tester.c
+++ get_next_line_tester/tester.c
@@ -6,7 +6,7 @@
 /*   By: eramanit <eramanit@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 21:27:29 by eramanit          #+#    #+#             */
-/*   Updated: 2025/04/01 07:34:08 by eramanit         ###   ########.fr       */
+/*   Updated: 2025/04/13 21:32:19 by eramanit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ int test1(void)
 	int		fd;
 	char	*s;
 
+	puts("\033[32m TEST 1 \033[00m");
 	fd = open("test", O_RDONLY);
 	if (!fd)
 		return (1);
@@ -34,6 +35,7 @@ int test1(void)
 		ft_display(s);
 		s = get_next_line(fd);
 	}
+	close(fd);
 	return (0);
 }
 
@@ -42,6 +44,7 @@ int test2(void)
 	int	fd;
 	char	*s;
 
+	puts("\033[32m TEST 1 \033[00m");
 	fd = 1;
 	s = get_next_line(fd);
 	ft_display(s);
@@ -54,6 +57,7 @@ int	test3(void)
 {
 	char	*s;
 
+	puts("\033[32m TEST 1 \033[00m");
 	s = get_next_line(5);
 	ft_display(s);
 	free(s);
