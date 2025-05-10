33760920ee281d16c02c8729054c27d2512f8ebb
Eddie Lantoniaina Ramanitrandrasana <eramanit@c1r11s2.42antananarivo.mg>
2025-05-10 10:24:18 +0300

    test de NULL paramater

--- ft_printf_tester/main.c
+++ ft_printf_tester/main.c
@@ -6,11 +6,12 @@
 /*   By: eramanit <eramanit@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/29 06:56:41 by eramanit          #+#    #+#             */
-/*   Updated: 2025/04/15 00:07:02 by eramanit         ###   ########.fr       */
+/*   Updated: 2025/05/10 10:22:41 by eramanit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "tester.h"
+
 int main(void)
 {
 	test_c();
@@ -23,5 +24,6 @@ int main(void)
 	test_X();
 	test_percent();
 	test_mix();
+	ft_putnbr_fd(ft_printf(NULL) == printf(NULL), 1);
 	return (0);
 }
